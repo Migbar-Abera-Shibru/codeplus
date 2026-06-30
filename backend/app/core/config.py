63b3d73cb0f05c9500b073dda23e14d965a5ca38
Migbar <aberamigbar@gmail.com>
2026-06-30 23:12:49 +0300

    added config.py and modified main file added requriements and dockerfile on frontend

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,12 +1,13 @@
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
-    #App
+    # App
     APP_NAME: str = "CodePlus"
     DEBUG: bool = False
     SECRET_KEY = str
 
-    #Database
+    # Database
     DATABASE_URL: str
 
     # Github OAuth
@@ -14,14 +15,15 @@ class Settings(BaseSettings):
     GITHUB_CLIENT_SECRET: str
     GITHUB_API_TOKEN: str  # personal token for higher rate limits
 
-    # Redis 
-    REDIS_URL: str= "redis://localhost:6379"
+    # Redis
+    REDIS_URL: str = "redis://localhost:6379"
 
     # Cache TTL (seconds)
-    PROFILE_CACHE_TTL: int= 3600 # 1 hour
-    REPO_CACHE_TTL: int=7200 #2 hours
+    PROFILE_CACHE_TTL: int = 3600  # 1 hour
+    REPO_CACHE_TTL: int = 7200  # 2 hours
 
     class Config:
         env_file = ".env"
+
 
 settings = Settings()
